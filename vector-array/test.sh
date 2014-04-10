ae25bd624023adc27e1e9bc5dfceaf2c527766ac
Stan Manilov <stanislav.manilov@gmail.com>
2014-04-10 12:09:35 +0100

    Fix previous commit: wrong filename

--- vector-array/test.sh
+++ vector-array/test.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 echo "Standard compilation (64-bit?)"
-g++ -std=c++98 tmp.cpp -o tmp
+g++ -std=c++98 vector-array.cpp -o tmp
 ./tmp 
  
 echo ""
 echo ""
 echo "32-bit"
-g++ -m32 -std=c++98 tmp.cpp -o tmp
+g++ -m32 -std=c++98 vector-array.cpp -o tmp
 ./tmp 
