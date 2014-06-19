e1ab1ab1bd772aa6306fe70b8d9c1328e05cd232
Stan Manilov <stanislav.manilov@gmail.com>
2014-06-19 15:35:11 +0100

    Golbach-peano: new data

--- goldbach_peano/goldbach_peano.cpp
+++ goldbach_peano/goldbach_peano.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 
 using std::stoi;
+using std::stod;
 
 using std::ostream;
 using std::cout;
@@ -75,26 +76,44 @@ bool isPrime(int x) {
 }
 
 bool drawSpiralPixel(PPMImage& image, int x0, int y0, double r, double theta,
-                double num, const RGB24bit& primeColor, const RGB24bit& compositeColor) {
+                int num, const RGB24bit& primeColor, const RGB24bit& compositeColor) {
         int x = (int) (x0 + r * cos(theta));
         int y = (int) (y0 + r * sin(theta));
-        if (isPrime((int)(num))) {
-                if (!image.setPixel(x, y, primeColor)) {
-                        return false;
+
+        RGB24bit evenColor = compositeColor;
+        evenColor.red   /= 2;
+        evenColor.green /= 2;
+        evenColor.blue  /= 2;
+
+        RGB24bit specColor = { 0xff, 0xff, 0x00 };
+
+        if (isPrime(num)) {
+                if (num % 10 == 7) {
+                        if (!image.setPixel(x, y, specColor)) {
+                                return false;
+                        }
+                } else {
+                        if (!image.setPixel(x, y, primeColor)) {
+                                return false;
+                        }
                 }
         } else {
-                if (!image.setPixel(x, y, compositeColor)) {
-                        return false;
+                if (num % 2 == 0) {
+                        if (!image.setPixel(x, y, evenColor)) {
+                                return false;
+                        }
+                } else {
+                        if (!image.setPixel(x, y, compositeColor)) {
+                                return false;
+                        }
                 }
         }
         return true;
 }
 
-void drawSpiral(PPMImage& image, int x0, int y0, int lineWidth, const RGB24bit& primeColor, const RGB24bit& compositeColor) {
-        double b = 2.0;
+void drawSpiral(PPMImage& image, int x0, int y0, double b, double scale, int lineWidth, const RGB24bit& primeColor, const RGB24bit& compositeColor) {
         double dtheta = 1e-1;
 
-        double scale = 0.01;
         for (double theta = 0.0; ; theta += dtheta) {
                 double r = b * theta;
                 double num = theta * r * scale;
@@ -136,37 +155,43 @@ int main(int argc, char **argv)
 
         int lineWidth = 5;
 
+        double b = 2.0;
+        double scale = 0.01;
+
         RGB24bit backgroundColor = {0xff, 0x11, 0x00};
         RGB24bit primeColor = {0xff, 0xff, 0x00};
         RGB24bit compositeColor = {0x00, 0x00, 0x00};
 
         if (argc > 1) {
-                if (argc != 13) {
+                if (argc != 15) {
                         usage(argc, argv);
                         return 1;
                 } else {
                         W                     = stoi(argv[1]);
                         H                     = stoi(argv[2]);
 
-                        lineWidth             = stoi(argv[3]);
+                        b                     = stod(argv[3]);
+                        scale                 = stod(argv[4]);
 
-                        backgroundColor.red   = stoi(argv[4]);
-                        backgroundColor.green = stoi(argv[5]);
-                        backgroundColor.blue  = stoi(argv[6]);
+                        lineWidth             = stoi(argv[5]);
 
-                        primeColor.red        = stoi(argv[7]);
-                        primeColor.green      = stoi(argv[8]);
-                        primeColor.blue       = stoi(argv[9]);
+                        backgroundColor.red   = stoi(argv[6]);
+                        backgroundColor.green = stoi(argv[7]);
+                        backgroundColor.blue  = stoi(argv[8]);
 
-                        compositeColor.red    = stoi(argv[10]);
-                        compositeColor.green  = stoi(argv[11]);
-                        compositeColor.blue   = stoi(argv[12]);
+                        primeColor.red        = stoi(argv[9]);
+                        primeColor.green      = stoi(argv[10]);
+                        primeColor.blue       = stoi(argv[11]);
+
+                        compositeColor.red    = stoi(argv[12]);
+                        compositeColor.green  = stoi(argv[13]);
+                        compositeColor.blue   = stoi(argv[14]);
                 }
         }
 
         PPMImage img(W, H, backgroundColor);
 
-        drawSpiral(img, W/2, H/2, lineWidth, primeColor, compositeColor);
+        drawSpiral(img, W/2, H/2, b, scale, lineWidth, primeColor, compositeColor);
 
         cout << img;
 }
